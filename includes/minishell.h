172ce1af09930cf4c602e0ed20da1bccef69ada4
JunseoHo <junseoho1029@gmail.com>
2023-08-25 17:13:14 +0900

    feat : ready

---
+++ includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/25 15:51:41 by jho               #+#    #+#             */
+/*   Updated: 2023/08/25 17:00:03 by jho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <dirent.h>
+# include <term.h>
+# include <sys/ioctl.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+#endif
--- sources/main/msh_main.c
+++ sources/main/msh_main.c
@@ -1,17 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   msh_main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/25 15:51:41 by jho               #+#    #+#             */
-/*   Updated: 2023/08/25 15:54:49 by jho              ###   ########.fr       */
+/*   Created: 2023/08/25 16:14:20 by jho               #+#    #+#             */
+/*   Updated: 2023/08/25 17:11:29 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# include <dirent.h>
-# include <>
-#endif
+#include "../../includes/minishell.h"
+
+int	main(void)
+{
+	char	*prompt;
+	char	*line;
+
+	prompt = "$> ";
+	while (1)
+	{
+		line = readline(prompt);
+		free(line);
+	}
+
+	return (0);
+}
