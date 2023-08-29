8965d1e747264e00a4edd54d0d55631bef5c752a
JunseoHo <junseoho1029@gmail.com>
2023-08-29 15:18:44 +0900

    feat : msh_lexical_analysis.c

--- sources/lexer/msh_lexical_analysis.c
+++ sources/lexer/msh_lexical_analysis.c
@@ -1,27 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   msh_main.c                                         :+:      :+:    :+:   */
+/*   msh_lexical_analysis.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/25 16:14:20 by jho               #+#    #+#             */
-/*   Updated: 2023/08/29 15:14:04 by jho              ###   ########.fr       */
+/*   Created: 2023/08/29 15:14:26 by jho               #+#    #+#             */
+/*   Updated: 2023/08/29 15:18:15 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	main(void)
+t_token	*msh_lexical_analysis(char *input)
 {
-	char	*input;
-	t_token	*symbol_list;
-
-	while (1)
-	{
-		input = readline("$> ");
-		symbol_list = msh_lexical_analysis(input);
-		free(input);
-	}
 	return (0);
 }
