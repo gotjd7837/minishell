5310c01a292ac412ee66163daf72e03f2425fc24
JunseoHo <junseoho1029@gmail.com>
2023-11-25 05:12:36 +0900

    fix : builtin

--- sources/msh_executor/msh_execute_first.c
+++ sources/msh_executor/msh_execute_first.c
@@ -6,7 +6,7 @@
 /*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:24:03 by jho               #+#    #+#             */
-/*   Updated: 2023/11/25 04:34:20 by jho              ###   ########.fr       */
+/*   Updated: 2023/11/25 05:06:52 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,24 @@
 
 int	msh_execute_first_builtin(t_pipeline *pl, int *fd, t_env *env, char *heredoc_list)
 {
-	char **param;
+	int		fd_cp[5];
+	char	**param;
 
-	if (fd[0] != 0 && close(fd[0]) == -1)
+	fd_cp[0] = 0;
+	fd_cp[1] = fd[1];
+	if (!msh_execute_redir(pl->tokens, fd_cp, heredoc_list))
 		return (0);
-	fd[0] = 0;
 	param = msh_token_filter_sym(pl->tokens, WORD);
 	if (!msh_execute_redir(pl->tokens, fd, heredoc_list))
 		return (0);
 	if (param == NULL)
 		return (0);
-	return (msh_execute_builtin(fd[0], fd[1], param, env));
+	msh_execute_builtin(fd_cp[0], fd_cp[1], param, env);
+	if (fd_cp[0] != 0)
+		close(fd_cp[0]);
+	if (fd_cp[1] != fd[1])
+		close(fd_cp[1]);
+	return (1);
 }
 
 int	msh_execute_first(t_pipeline *pl, int *fd, t_env *env, char *heredoc_list)
--- sources/msh_executor/msh_execute_last.c
+++ sources/msh_executor/msh_execute_last.c
@@ -6,7 +6,7 @@
 /*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:25:16 by jho               #+#    #+#             */
-/*   Updated: 2023/11/25 04:51:50 by jho              ###   ########.fr       */
+/*   Updated: 2023/11/25 05:08:38 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 int	msh_execute_last_builtin(t_pipeline *pl, int *fd, t_env *env, char *heredoc_list)
 {
+	int		fd_cp[5];
 	int		result;
 	char	**param;
 
-	if (fd[1] != 1 && close(fd[1]) == -1)
-		return (0);
-	fd[1] = 1;
+	fd_cp[0] = fd[0];
+	close(fd[1]);
+	fd_cp[1] = 1;
+	if (!msh_execute_redir(pl->tokens, fd_cp, heredoc_list))
+		exit(errno);
 	param = msh_token_filter_sym(pl->tokens, WORD);
-	if (!msh_execute_redir(pl->tokens, fd, heredoc_list))
-		return (0);
 	if (param == NULL)
 		return (0);
 	result = msh_execute_builtin(fd[0], fd[1], param, env);
-	if (fd[0] != 0)
-		close(fd[0]);
-	if (fd[1] != 1)
-		close(fd[1]);
+	close(fd[1]);
+	close(fd[2]);
+	close(fd_cp[0]);
+	if (fd_cp[1] != 1)
+		close(fd_cp[1]);
 	return (result);
 }
 
