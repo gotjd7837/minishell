9b2f8b29d238da83ef8e530399e2461711de90e8
JunseoHo <junseoho1029@gmail.com>
2023-12-05 17:05:55 +0900

    fix : last exit code

--- includes/msh_executor.h
+++ includes/msh_executor.h
@@ -6,7 +6,7 @@
 /*   By: jho <jho@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 15:00:43 by jho               #+#    #+#             */
-/*   Updated: 2023/12/05 11:05:21 by jho              ###   ########.fr       */
+/*   Updated: 2023/12/05 16:59:55 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int		msh_execute_check_builtin(t_pipeline *pipeline);
 int		msh_execute_first(t_pipeline *pl, int *fd, t_env *env, int forked);
 void	msh_execute_free_param(char **param);
 int		msh_execute_last(t_pipeline *pl, int *fd, t_env *env);
-int		msh_execute_middle(t_pipeline *pl, int *fd, t_env *env, int *total);
+int		msh_execute_middle(t_pipeline *pl, int *fd, t_env *env);
 char	*msh_execute_mktemp(void);
 int		msh_execute_pipe_shift(int *fd, int *local_fd);
 void	msh_execute_pipeline(int in, int out, char **param, t_env *env);
