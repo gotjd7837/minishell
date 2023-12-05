2a8dff16456627219d182c6f37d0c2d74209f4d1
JunseoHo <junseoho1029@gmail.com>
2023-12-05 10:54:55 +0900

    fix : tokenize qoute

--- sources/msh_lexer/msh_lex_tokenize_qoutes.c
+++ sources/msh_lexer/msh_lex_tokenize_qoutes.c
@@ -6,33 +6,48 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 08:49:45 by jho               #+#    #+#             */
-/*   Updated: 2023/12/04 15:02:42 by haekang          ###   ########.fr       */
+/*   Updated: 2023/12/05 10:54:20 by jho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/msh_lexer.h"
 
+int	msh_lex_tokenize_qoutes_find_last_idx(char *s)
+{
+	int	qoute_idx;
+	int	last_idx;
+
+	qoute_idx = 0;
+	last_idx = 0;
+	while (!msh_is_charset(*(s + last_idx), "|<> ") && *(s + last_idx) != '\0')
+	{
+		if (msh_is_charset(*(s + last_idx), "\'\""))
+		{
+			qoute_idx = last_idx++;
+			while (*(s + last_idx) != *(s + qoute_idx))
+				++last_idx;
+		}
+		++last_idx;
+	}
+	return (last_idx);
+}
+
 int	msh_lex_tokenize_qoutes(t_token **sym_table, char *s)
 {
-	int		idx;
+	int		last_idx;
 	t_token	*token;
 
-	idx = 1;
-	while (*(s + idx) != *s)
-		++idx;
-	++idx;
-	while (!msh_is_charset(*(s + idx), "&|<>() ") && *(s + idx) != '\0')
-		++idx;
+	last_idx = msh_lex_tokenize_qoutes_find_last_idx(s);
 	token = msh_token_malloc_sym(WORD);
 	if (token == NULL)
 		return (0);
-	// token->val = msh_substr(s, 0, idx);
-	token->val = msh_substr(s, 1, idx - 1);
+	token->val = msh_substr(s, 1,
+			last_idx - msh_is_charset(*(s + last_idx - 1), "\'\""));
 	if (token->val == NULL)
 	{
 		msh_token_free(token);
 		return (0);
 	}
 	msh_token_add_next(sym_table, token);
-	return (idx);
+	return (last_idx);
 }
